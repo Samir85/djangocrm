24c62b08dc88c9c0802e2ad76dbce7e1b32618c1
Samir85 <rakroukisamir0@gmail.com>
2023-06-18 13:17:23 +0100

    update models

--- website/models.py
+++ website/models.py
@@ -12,6 +12,6 @@ class Record(models.Model):
     state = models.CharField(max_length=50)
     zipcode = models.CharField(max_length=30)
 
-def __str__(self):
-    return (f'{self.first_name} {self.last_name}')
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")
 
