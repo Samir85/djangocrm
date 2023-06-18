915c539651bf2360266f4a3f2d3db63c7fe9b329
Samir85 <rakroukisamir0@gmail.com>
2023-06-18 13:04:05 +0100

    update view

--- website/views.py
+++ website/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 # Create your views here.
 def home(request):
+    record = Record.objects.all()
     # check to see if login 
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +22,7 @@ def home(request):
             messages.success(request, 'there was an error , Try again !')
             return redirect('home')
     else:
-        return render (request, 'home.html' , {})
+        return render (request, 'home.html' , {'records':record})
     
 def logout_user(request):
     logout(request)
