13f0d4a56ac6385206e751efe76a59a47a7687be
Samir85 <rakroukisamir0@gmail.com>
2023-06-26 15:48:43 +0100

    update

--- website/forms.py
+++ website/forms.py
@@ -32,13 +32,13 @@ class SignUpForm(UserCreationForm):
 
 #  Add record form 
 class AddRecordForm(forms.ModelForm):
-    first_name= forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'first_name', "class":'form-control'}), label="")
-    last_name= forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'last_name', "class":'form-control'}), label="")
-    email= forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'email', "class":'form-control'}), label="")
-    phone= forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'phone', "class":'form-control'}), label="")
-    city= forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'city', "class":'form-control'}), label="")
-    state= forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'state', "class":'form-control'}),label="")
-    zipcode= forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'zipcode', "class":'form-control'}), label="")
+    first_name= forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={'placeholder':'first_name', "class":'form-control'}), label="")
+    last_name= forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={'placeholder':'last_name', "class":'form-control'}), label="")
+    email= forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={'placeholder':'email', "class":'form-control'}), label="")
+    phone= forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={'placeholder':'phone', "class":'form-control'}), label="")
+    city= forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={'placeholder':'city', "class":'form-control'}), label="")
+    state= forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={'placeholder':'state', "class":'form-control'}),label="")
+    zipcode= forms.CharField(required=False, widget=forms.widgets.TextInput(attrs={'placeholder':'zipcode', "class":'form-control'}), label="")
     
     class Meta:
         model = Record
