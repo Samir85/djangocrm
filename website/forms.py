9c50495a69ff8cd364c06fd09ff9344f38ee2549
Samir85 <rakroukisamir0@gmail.com>
2023-06-26 15:24:16 +0100

    update

--- website/forms.py
+++ website/forms.py
@@ -6,16 +6,13 @@ class SignUpForm(UserCreationForm):
     email = forms.EmailField(label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Email'}))
     first_name = forms.CharField(label ='', max_length='100',widget=forms.TextInput(attrs={'class':'form-control','placeholder':'first_name'}))
     last_name = forms.CharField(label='',max_length='100',widget=forms.TextInput(attrs={'class':'form-control','placeholder':'last_name'}))
-
-
     class Meta:
-        model = User()
+        model = User
         fields = ('username','first_name','last_name','email','password1','password2')
 
-def __init__(self, *args, **kwargs):
-            
+    def __init__(self, *args, **kwargs):
+                
         super(SignUpForm, self).__init__(*args, **kwargs)
-
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'User Name'
         self.fields['username'].label = ''
@@ -30,7 +27,7 @@ def __init__(self, *args, **kwargs):
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
-    
+        
 
 
 #  Add record form 
